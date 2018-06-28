300bab05e86d50830c8a9ed085789951d03e16fa
SabalaPeople <sabalard01@gmail.com>
2018-06-28 13:26:50 +0800

    update loading.js

--- loading.js
+++ loading.js
@@ -7,13 +7,27 @@
 		let result = pathName.substr(0,index+1);
 	    
 		let imgPath = result + '/img/ajax-loader.gif';
-		let str = '';
-		str += '<div class="modal">';
-		str += '<div align="center" style="height:50%;"></div>';
-		str += '<div align="center" id="waitingMsg"></div>';
-		str += '<div><img src="' + imgPath + '"/></div>';
-		str += '</div>';
 		
-		document.body.innerHTML += str;
+		let modalDiv = document.createElement('div');
+		modalDiv.classList.add('modal');
 		
+		let blankDiv = document.createElement('div');
+		blankDiv.setAttribute('align', 'center');
+		blankDiv.setAttribute('style', 'height:50%;');
+		
+		let msgDiv = document.createElement('div');
+		msgDiv.setAttribute('id','waitingMsg');
+		msgDiv.setAttribute('align','center');
+		
+		let img = document.createElement('img');
+		img.setAttribute('src', imgPath);
+		
+		let imgDiv = document.createElement('div');
+		imgDiv.appendChild(img);
+		
+		modalDiv.appendChild(blankDiv);
+		modalDiv.appendChild(msgDiv);
+		modalDiv.appendChild(imgDiv);
+		
+		document.body.appendChild(modalDiv);
 	}
